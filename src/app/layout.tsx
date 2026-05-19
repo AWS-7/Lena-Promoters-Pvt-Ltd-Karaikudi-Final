fe1ed4d6816d7c4e233d225353260af1751e4ebe
AWS-TEAM <agniwebsolution@gmail.com>
2026-05-19 18:54:34 +0530

    files upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,15 +87,24 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://rjeydmqspxklsrtohumn.supabase.co" />
         <link rel="dns-prefetch" href="https://rjeydmqspxklsrtohumn.supabase.co" />
-        {/* Google tag (gtag.js) */}
+        {/* Google tag (gtag.js) - AW-18145943083 */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18145943083"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-18145943083');`,
-          }}
-        />
+        <script id="gtag-init">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-18145943083');`}
+        </script>
+        {/* Google Tag Manager */}
+        <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P8LR9ZRP');`}</script>
       </head>
       <body className={`${poppins.variable} ${inter.variable} min-h-full flex flex-col pb-[88px] md:pb-0`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P8LR9ZRP" height="0" width="0" style="display:none;visibility:hidden"></iframe>` }} />
+        {/* End Google Tag Manager (noscript) */}
         <VisitorTracker />
         <WelcomeSound />
         <Preloader />
