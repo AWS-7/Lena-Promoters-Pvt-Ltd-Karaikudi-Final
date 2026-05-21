5fc849f9bddb59e97a2b43e20377de533e4971d8
AWS-TEAM <agniwebsolution@gmail.com>
2026-05-21 20:23:20 +0530

    files upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -86,8 +86,8 @@ export default function RootLayout({
       <head>
         {/* Favicons */}
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel="icon" type="image/jpeg" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/jpeg" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
         <link rel="apple-touch-icon" sizes="192x192" href="/android-chrome-192x192.png" />
         <link rel="manifest" href="/manifest.json" />
 
